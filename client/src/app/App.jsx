b028cfecd7d4a25d60118b73d7e7ae5828debf37
Jordan Iralde <iraldejordan10@gmail.com>
2025-01-29 17:03:07 -0300

    Main fixed unless sections

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -15,7 +15,6 @@ export default function App() {
         <Login />
         <TarjetaPago />
         <Main />
-        <div>App</div>
         <Footer />
       </>
     );
--- client/src/app/pages/main.jsx
+++ client/src/app/pages/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; // Elimina useState si no se usa
 
-import './mainStyles.css'
+import styles from'./main.module.css'
 
 import Section01 from './componentsMain/section-01/section-01';
 import Section02 from './componentsMain/section-02/section-02';
@@ -8,14 +8,15 @@ import Section03 from './componentsMain/section-03/section-03';
 
 const Main = () => {
   return (
-    <div className='main'>
-      <div className='background-main-01'>
-        <div className='inner-container'>
+    <div className={styles.main}>
+      <div className={styles.backgroundMain}>
+        <div className={styles.innerContainer}>
           <Section01 />
         </div>
-          <Section02 />
-          <Section03 />
+
       </div>
+      <Section02 />
+      <Section03 />
     </div>
   );
 };
