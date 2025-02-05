d05b561d03254f9777eb7294519a49c7b65402c7
Jordan Iralde <iraldejordan10@gmail.com>
2025-02-05 12:02:34 -0300

    Estilos

--- client/src/app/pages/componentsMain/section-01/section-01.jsx
+++ client/src/app/pages/componentsMain/section-01/section-01.jsx
@@ -7,14 +7,33 @@ export default function Section01() {
             <h1 className={styles.title}>Películas y series ilimitadas y mucho más</h1>
             <p className={styles.subtitle}>A partir de US$ 9.99. Cancela cuando quieras.</p>
             <div className={styles.contentEmail}>
-                <p className={styles.description}>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reactivar tu membresía de Netflix.</p>
-                <div className={styles.inputContainer}>
-                    <input className={styles.input} type="email" placeholder="Email" />
-                    <button className={styles.button}>
+                <p className={styles.description}>
+                    ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reactivar tu membresía de Netflix.
+                </p>
+                <form className={styles.form}>
+                    <input 
+                        type="email" 
+                        placeholder="Email" 
+                        className={styles.input} 
+                    />
+                    <button type="submit" className={styles.button}>
                         Comenzar
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"></path></svg>
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            width="24" 
+                            height="24" 
+                            viewBox="0 0 24 24" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            strokeWidth="2" 
+                            strokeLinecap="round" 
+                            strokeLinejoin="round" 
+                            className={styles.icon}
+                        >
+                            <path d="m9 18 6-6-6-6"></path>
+                        </svg>
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     );
